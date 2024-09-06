fcb64bd8653044cab489b46dd686908f7b3969d4
Grass <grasshong@mayaminer.com>
2024-09-06 17:59:06 +0800

    test

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./index.css"
+import nav from"./menu.js"
+import welcom from "./welcom.js"
+import main from "./main.js"
+
+document.body.appendChild(nav);
+document.body.appendChild(welcom);
+document.body.appendChild(main);
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./main.css"
+
+let main = document.createElement("main");
+main.textContent = "Main Content";
+
+export default main;
