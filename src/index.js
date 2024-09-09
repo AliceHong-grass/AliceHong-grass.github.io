ef46bb5459317fddd666551318b8a941f35315b7
Grass <grasshong@mayaminer.com>
2024-09-09 11:11:50 +0800

    test

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./index.css"
-import nav from"./menu.js"
-import welcom from "./welcom.js"
-import main from "./main.js"
-
-document.body.appendChild(nav);
-document.body.appendChild(welcom);
-document.body.appendChild(main);
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import "./main.css"
-
-let main = document.createElement("main");
-main.textContent = "Main Content";
-
-export default main;
